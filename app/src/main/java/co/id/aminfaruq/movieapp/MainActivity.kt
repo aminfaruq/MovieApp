e570685087700acae83872951e578aed62d5aa57
uwaisalqadri <uwaisalqadri654321@gmail.com>
2020-09-23 22:00:07 +0700

    add tabs, viewpagers, and noActionBar Style

--- app/src/main/java/co/id/aminfaruq/movieapp/MainActivity.kt
+++ app/src/main/java/co/id/aminfaruq/movieapp/MainActivity.kt
@@ -2,10 +2,19 @@ package co.id.aminfaruq.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import co.id.aminfaruq.movieapp.ui.adapter.SectionPagerAdapter
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sectionPagerAdapter = SectionPagerAdapter(this, supportFragmentManager)
+
+        view_pager.adapter = sectionPagerAdapter
+        tabs.setupWithViewPager(view_pager)
     }
 }
