01d048c03291224772993a0975a2f93b69ddfe9a
Faruk Amin <aminfaruk.fa@gmail.com>
2020-09-30 13:17:49 +0800

    added new activity

---
+++ detail/src/main/java/co/id/aminfaruq/movieapp/detail/di/detailModule.kt
@@ -0,0 +1,2 @@
+package co.id.aminfaruq.movieapp.detail.di
+
---
+++ detail/src/main/java/co/id/aminfaruq/movieapp/detail/ui/DetailActivity.kt
@@ -0,0 +1,12 @@
+package co.id.aminfaruq.movieapp.detail.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.id.aminfaruq.movieapp.detail.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
